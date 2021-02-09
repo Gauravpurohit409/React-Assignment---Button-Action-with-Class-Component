35c946c5c919f9b2925c0ad56fa1c902c33cbb94
Gauravpurohit409 <gauravpurohit6666@gmail.com>
2021-02-09 19:11:12 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React, {Component, useState} from "react";
+import { render } from "react-dom";
 import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
 		super(props);
-	};
 
+		this.state = ({
+			visibility : "none",
+		})
+	}
+	
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				<p id= "para" style = {{display: this.state.visibility}}>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+				<button id = "click" onClick ={() =>{
+					this.setState({
+						visibility : "block",
+					});
+
+				}}>Click!</button>
     		</div>
     	);
     }
@@ -17,4 +29,5 @@ class App extends Component {
 
 
 export default App;
+
 
